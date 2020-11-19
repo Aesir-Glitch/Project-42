a63bf8ed46c0eabfe495f7f113ebe4c1b9cec142
Aesir-Glitch <65230467+Aesir-Glitch@users.noreply.github.com>
2020-11-18 18:18:49 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,13 +5,16 @@ var sc = second();
 function setup() {
   createCanvas(800,400);
   createSprite(400, 200, 50, 50);
+  scAngle = map(sc,0,60,0,360);
+  mnAngle = map(mn,0,40,0,360);
+  hrAngle = map(hr,0,80,0,360);
+
 }
 
 function draw() {
   background(255,255,255);
   angleMode(DEGREES);
   
-  scAngle = map(sc,0,60,0,360);
   push();
   rotate(scAngle);
   stroke(255,0,0);
@@ -19,7 +22,6 @@ function draw() {
   line(0,0,100,0);
   pop();
 
-  mnAngle = map(mn,0,40,0,360);
   push();
   rotate(mnAngle);
   stroke(255,0,0);
@@ -27,7 +29,6 @@ function draw() {
   line(0,0,100,0);
   pop();
 
-  hrAngle = map(hr,0,80,0,360);
   push();
   rotate(hrAngle);
   stroke(255,0,0);
